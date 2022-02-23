8eb6c5e9fc820f9bdb7ce4e524d4d341b0dd9563
Hisham Salameh <salamehh@msu.edu>
2022-02-23 09:02:04 -0500

    changes made

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const button = document.querySelector('.btn')
 button.addEventListener('click', getQuote)
 
 function getQuote() {
+  // FETCH THE API
   fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes/5')
+    // CONVERT THE RESPONSE TO JSON
     .then(response => response.json())
     .then(people => {
+      // ITERATE THROUGH JSON OBJECTS AND TAKE DATA WE WANT
       people.forEach(person => {
         quotes.textContent = `"${person.quote}"`
         author.textContent = `- ${person.author}`
